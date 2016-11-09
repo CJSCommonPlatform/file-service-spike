e237b9a62e431ee5d189657c0c893fd050387ac4
Pradeep Thomas <pradeep.thomas@hmcts.net>
2016-11-09 15:38:12 +0000

    Add Consistent Hash

--- file-service-core/src/test/java/uk/gov/justice/services/fileservice/FileLookupTest.java
+++ file-service-core/src/test/java/uk/gov/justice/services/fileservice/FileLookupTest.java
@@ -1,20 +1,40 @@
 package uk.gov.justice.services.fileservice;
 
-import java.util.UUID;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.Assert;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.junit.Test;
 
 public class FileLookupTest {
 
-
     @Test
-    public void testGetFileName() {
-        final DirectoryPath dp = new DirectoryPath();
-        final FileLookup fl = new FileLookup(dp);
+    public void shouldLookupFileName() {
         final UUID uuid = UUID.randomUUID();
-        final String fileName = fl.getFileName(uuid);
-        Assert.assertTrue(fileName, fl.getFileName(uuid).matches("/gluster-storage/dir-[0-9]"));
+        final String fileName = new FileLookup(new DirectoryPath()).getFileName(uuid);
+        assertTrue(fileName, fileName.matches(
+                        String.join("","/gluster-storage/dir-[0-9]/", uuid.toString())));
     }
+    
+    @Test
+    public void shouldTestConsistentLookupFileName() {
+        final FileLookup fileLookup = new FileLookup(new DirectoryPath());
+        final Map<UUID, String> fileNames = new TreeMap<>();
+        final List<UUID> randomUUIDs = Stream.generate(UUID::randomUUID).limit(100000)
+                        .collect(Collectors.toList());
 
+        randomUUIDs.forEach(uuid -> {
+            fileNames.put(uuid, fileLookup.getFileName(uuid));
+        });
+
+        for(Map.Entry<UUID, String> uuidEntry: fileNames.entrySet()){
+            assertEquals(uuidEntry.getValue(), fileLookup.getFileName(uuidEntry.getKey()));
+        }        
+    }
 }
